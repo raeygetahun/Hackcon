0f26bb3efe2141fa6e8eff50a2777ab8f024f207
ereminaeliz <eremina.eliz2014@gmail.com>
2023-06-24 16:03:15 +0200

    add first sketch with initial screen and all the styles

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <nav className="navbar">
+                    <div className="logo">HACKCON</div>
+                    <ul className="nav-links">
+                        <li>About</li>
+                        <li>Speakers</li>
+                        <li>Schedule</li>
+                        <li>Team</li>
+                        <li>FAQ</li>
+                        <li>Contact</li>
+                    </ul>
+                </nav>
+                <section className="welcome-section">
+                    <h1>JOIN US AT HACKCON<br/>2023</h1>
+                    <p className="subtext">Embrace Cutting-Edge Collaborations and Experience<br/>the Future of AI</p>
+                </section>
+            </header>
+        </div>
+    );
 }
 
 export default App;
