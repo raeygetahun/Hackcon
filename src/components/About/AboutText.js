839f5cea5b71b21e3091180683b4059aa5e00dd6
raeygetahun <103925724+raeygetahun@users.noreply.github.com>
2023-06-25 19:33:10 +0200

    Update AboutText.js

--- src/components/About/AboutText.js
+++ src/components/About/AboutText.js
@@ -1,1 +1,69 @@
+import React, { useEffect, useState }  from "react";
+import { Grid } from "@mui/material";
+import hero_hackathons from '../../assets/hero_hackathons.jpg'//image subject to change
+import "./about.css";
 
+
+const AboutText = () => {
+  const [isLineActive, setIsLineActive] = useState(false);
+
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const shortLine = document.querySelector(".ShortLine");
+
+
+      if (shortLine) {
+        const shortLineTop = shortLine.getBoundingClientRect().top;
+        const windowHeight = window.innerHeight;
+
+
+        setIsLineActive(shortLineTop <= windowHeight * 0.8);
+      }
+    };
+
+
+    window.addEventListener("scroll", handleScroll);
+
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+  return (
+    <div className="About ">
+      <div style={{ backgroundColor: "white", height: "500px" }}></div>
+      <Grid container spacing={3}>
+        <Grid className="AboutText" item md={6} xs={12}>
+          <div className="VerticalLine"></div>
+          <div className={`ShortLine ${isLineActive ? "active" : ""}`}></div>
+          <div className="AboutTextWrapper">
+            <h4>ABOUT HACKCON</h4>
+            <span>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              Tristique senectus et netus et malesuada fames ac. Venenatis
+              tellus in metus vulputate eu scelerisque felis imperdiet proin.
+              Volutpat commodo sed egestas egestas fringilla phasellus faucibus
+              scelerisque. Nunc sed id semper risus in hendrerit gravida rutrum
+              quisque. Eu mi bibendum neque egestas congue quisque egestas diam.
+              Blandit massa enim nec dui. Pretium vulputate sapien nec sagittis
+              aliquam malesuada bibendum arcu. Magnis dis parturient montes
+              nascetur ridiculus. Posuere morbi leo urna molestie at elementum
+              eu facilisis. Enim nulla aliquet porttitor lacus luctus accumsan.
+              In fermentum et sollicitudin ac orci phasellus egestas. Tellus
+              integer feugiat scelerisque varius morbi enim. Neque sodales ut
+              etiam sit amet nisl. Egestas integer eget aliquet nibh.
+            </span>
+          </div>
+        </Grid>
+        <Grid item md={6}>
+          <img src={hero_hackathons} style={{ maxWidth: "100%",height:'100%' }} />
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
+
+
+export default AboutText;
