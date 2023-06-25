69519be7edf065dee58fef8e7e85ba308d731ef9
raeygetahun <103925724+raeygetahun@users.noreply.github.com>
2023-06-25 20:26:40 +0200

    added HackconInNumbers

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
-import React from 'react';
+import React from "react";
 import Navbar from "./components/Navbar/Navbar";
 import Welcome from "./components/Welcome/Welcome";
 import AboutText from "./components/About/AboutText";
 import "./App.css";
 import WhyHackCon from "./components/About/WhyHackCon/WhyHackCon";
 import Address from "./components/Contact/Contact";
+import HackconInNumbers from "./components/About/HackconInNumbers";
 
 function App() {
   return (
     <div className="App">
-            <header className="App-header">
-                <Navbar/>
-                <Welcome/>
-                <AboutText/>
-                <Address/>
-                <WhyHackCon/>
-            </header>
+      <header className="App-header">
+        <Navbar />
+        <Welcome />
+        <AboutText />
+        <HackconInNumbers />
+        <Address />
+        <WhyHackCon />
+      </header>
       <main>
         <WhyHackCon />
         <Address />
