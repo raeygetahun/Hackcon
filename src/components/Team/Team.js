949992c5d0ec3b6973ce74131dd278b6a5e1a110
ereminaeliz <eremina.eliz2014@gmail.com>
2023-06-27 21:02:14 +0200

    add "Our Team" section

--- src/components/Team/Team.js
+++ src/components/Team/Team.js
@@ -1,5 +1,8 @@
 import React from 'react'
-import firstScreenImage from "../../assets/first-screen.png";
+import teamMemberImage from "../../assets/team-member.jpg";
+import teamMember2Image from "../../assets/team-member2.jpg";
+import teamMember3Image from "../../assets/team-member3.jpg";
+import teamMember4Image from "../../assets/team-member4.jpg";
 import './team.css'
 
 const
@@ -7,13 +10,38 @@ const
   return (
       <section id="team-section" className="team-section">
           <div className="text-container-team">
-            <h1>OUR TEAM</h1>
+            <h>OUR TEAM</h>
           </div>
           <div className="image-container-team">
-            <img src={firstScreenImage} alt="about"/>
-            {/*<img src={firstScreenImage} alt="about"/>*/}
-            {/*<img src={firstScreenImage} alt="about"/>*/}
-            {/*<img src={firstScreenImage} alt="about"/>*/}
+              <div className="team-member">
+                  <img src={teamMemberImage} alt="team-member 1"/>
+                  <div className="name-overlay">
+                      <h1>Dakota Johnson</h1>
+                      <p>actress</p>
+                  </div>
+              </div>
+              <div className="team-member">
+                  <img src={teamMember2Image} alt="team-member 2"/>
+                  <div className="name-overlay">
+                      <h1>Emma Watson</h1>
+                      <p>actress</p>
+                  </div>
+              </div>
+              <div className="team-member">
+                  <img src={teamMember3Image} alt="team-member 3"/>
+                  <div className="name-overlay">
+                      <h1>Some girl</h1>
+                      <p>random</p>
+                  </div>
+              </div>
+              <div className="team-member">
+                  <img src={teamMember4Image} alt="team-member 4"/>
+                  <div className="name-overlay">
+                      <h1>Daniel Radcliffe</h1>
+                      <p>actor</p>
+                  </div>
+              </div>
+              {/*Scale if you wish*/}
           </div>
       </section>
   )
