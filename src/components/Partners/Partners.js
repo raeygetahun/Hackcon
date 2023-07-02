d1da54e6a6153e530e53ccbe54d673b65cc230a6
raeygetahun <103925724+raeygetahun@users.noreply.github.com>
2023-07-02 21:52:20 +0200

    fixed partners comp

--- src/components/Partners/Partners.js
+++ src/components/Partners/Partners.js
@@ -1,48 +1,129 @@
-import React, {useEffect, useRef} from 'react'
-import firstScreenImage from '../../assets/first-screen.png';
-import './partners.css'
+import React, { useEffect, useState, useRef } from "react";
+import usgLogo from "../../assets/partners/usgLogo.jpeg";
+import nehlsen from "../../assets/partners/nehlsen.png";
+import Makersite_logo from "../../assets/partners/Makersite_logo.svg";
+import microsoft from "../../assets/partners/microsoft.svg";
+import gsa_logo from "../../assets/partners/gsa_logo.png";
+import Yelp from "../../assets/partners/Yelp_fullcolor_lightbg_RGB.png";
+import "./partners.css";
+import { Grid } from "@mui/material";
+import useScrollHandler from "../../hooks/useScrollHandler";
+
+const imageSources = [
+  "https://upload.wikimedia.org/wikipedia/commons/2/2f/Google_2015_logo.svg",
+  "https://s3.amazonaws.com/challengepost/sponsors/logos/000/001/630/highres/Twilio_logo_red.png",
+  Makersite_logo,
+  "https://upload.wikimedia.org/wikipedia/commons/9/91/Logo_of_Red_bull.svg",
+  usgLogo,
+  Yelp,
+  "https://upload.wikimedia.org/wikipedia/commons/9/94/Skyscanner_Logo_LockupHorizontal_SkyBlue_RGB.svg",
+  microsoft,
+  nehlsen,
+];
+
+const linkSources = [
+  "https://www.google.com",
+  "https://www.twilio.com",
+  "https://makersite.io/", 
+  "https://www.redbull.com",
+  "https://www.usg.com",
+  "https://www.nehlsen.com",
+  "https://www.microsoft.com",
+  "https://www.yelp.com",
+  "https://www.skyscanner.com",
+];
+
+const PartnersElement = ({ index, isLineActive }) => {
+  const [isActive, setIsActive] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsActive(isLineActive);
+    }, index * 500);
+
+    return () => clearTimeout(timer);
+  }, [index, isLineActive]);
+
+  return (
+    <Grid item xs={4} key={index}>
+      {linkSources[index] ? (
+        <a href={linkSources[index]} target="_blank" rel="noopener noreferrer">
+          <img
+            className={`partnersimage ${isActive ? "active" : ""}`}
+            src={imageSources[index]}
+            alt="about"
+          />
+        </a>
+      ) : (
+        <img
+          className={`partnersimage ${isActive ? "active" : ""}`}
+          src={imageSources[index]}
+          alt="about"
+        />
+      )}
+    </Grid>
+  );
+};
 
 const Partners = () => {
-    const movingElementPartnersRef = useRef(null);
+  const isLineActive = useScrollHandler("partners-section");
+  const movingElementPartnersRef = useRef(null);
 
-    useEffect(() => {
-        const handleScroll = () => {
-            const section = document.getElementById('welcome-section');
-            const sectionTop = section.offsetTop;
-            const sectionBottom = (sectionTop + section.offsetHeight) / 2;
+  useEffect(() => {
+    const handleScroll = () => {
+      const section = document.getElementById("welcome-section");
+      const sectionTop = section.offsetTop;
+      const sectionBottom = (sectionTop + section.offsetHeight) / 2;
 
-            if (window.scrollY >= sectionBottom) {
-                movingElementPartnersRef.current.classList.add('moving-element-partners');
-            } else {
-                movingElementPartnersRef.current.classList.remove('moving-element-partners');
-            }
-        };
+      if (window.scrollY >= sectionBottom) {
+        movingElementPartnersRef.current.classList.add(
+          "moving-element-partners"
+        );
+      } else {
+        movingElementPartnersRef.current.classList.remove(
+          "moving-element-partners"
+        );
+      }
+    };
 
-        window.addEventListener('scroll', handleScroll);
+    window.addEventListener("scroll", handleScroll);
 
-        return () => {
-            window.removeEventListener('scroll', handleScroll);
-        };
-    }, []);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
-    return (
-        <section id="partners-section" className="partners-section">
-            <div className="content-container-partners">
-                <div className="line-partners">
-                    <div className="moving-element-partners" ref={movingElementPartnersRef}></div>
-                </div>
-                <div className="text-container-partners">
-                    <h1>OUR PARTNERS</h1>
-                    <br/>
-                    <p>We are proud to partner with some of the most innovative companies and organizations in the field
-                        of <strong>AI</strong>.</p>
-                </div>
-                <div className="image-container-partners">
-                    <img src={firstScreenImage} alt="about"/>
-                </div>
-            </div>
-        </section>
-    )
-}
+  return (
+    <section id="partners-section" className="partners-section">
+      <div className="content-container-partners">
+        <div className="line-partners">
+          <div
+            className="moving-element-partners"
+            ref={movingElementPartnersRef}
+          ></div>
+        </div>
+        <div className="text-container-partners">
+          <h1>OUR PARTNERS</h1>
+          <br />
+          <p>
+            We are proud to partner with some of the most innovative companies
+            and organizations in the field of <strong>AI</strong>.
+          </p>
+        </div>
+        <div className="image-container-partners">
+          <Grid container spacing={3}>
+            {imageSources.map((_, index) => (
+              <PartnersElement
+                key={index}
+                index={index}
+                isLineActive={isLineActive}
+              />
+            ))}
+          </Grid>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default Partners
+export default Partners;
