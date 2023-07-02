ea9cc82a746da238c5e8e0ade111049f8faa3ffe
raeygetahun <103925724+raeygetahun@users.noreply.github.com>
2023-07-02 14:25:21 +0200

    updated who and what

---
+++ src/components/WhatAndWho/WhoCanParticipate.js
@@ -0,0 +1,49 @@
+import React from "react";
+import { Grid } from "@mui/material";
+import "./WhatAndWho.css";
+import useScrollHandler from "../../hooks/useScrollHandler";
+import { FaArrowLeft } from 'react-icons/fa';
+function WhoCanParticpate() {
+  const isLineActive = useScrollHandler("WhoCanParticpate");
+  return (
+    <div className="WhoCanParticpate">
+      <div className="WhoCanParticpateHeader">Who can Particpate</div>
+      <div
+        style={{ width: "40%", backgroundColor: "#ffff" }}
+        className="HorizontalLine"
+      ></div>
+      <div
+        style={{ width: "7%", left: "165px", borderTop: "3px solid #ffff" }}
+        className={`ShortHorizontalLine ${isLineActive ? "active" : ""}`}
+      ></div>
+      <div className="WhoCanParticpateWrapper">
+        <Grid container spacing={3}>
+          <Grid item md={6}>
+            <p className="FirstParagraph">
+              Hackcon is open to students from all over Europe who are
+              passionate about technology and innovation. There are no specific
+              requirements for participation, other than a willingness to learn
+              and collaborate with others. Whether you are a computer science
+              major or a creative writing student, there is a place for you at
+              Hackcon.
+            </p>
+          </Grid>
+          <Grid item md={6}>
+            <p className="SecondParagraph">
+              {" "}
+              Come join us and be a part of the next generation of innovators
+              and entrepreneurs! Click the button below to apply now.
+              <br />
+              <button className="apply-button">
+                Apply
+                <FaArrowLeft className="arrow-icon" />
+              </button>
+            </p>
+          </Grid>
+        </Grid>
+      </div>
+    </div>
+  );
+}
+
+export default WhoCanParticpate;
