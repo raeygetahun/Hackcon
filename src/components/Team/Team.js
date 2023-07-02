4ffe2dcb792ef0738ef2c35c3ad9ff28019afed9
raeygetahun <103925724+raeygetahun@users.noreply.github.com>
2023-07-02 23:27:30 +0200

    updated team comp

--- src/components/Team/Team.js
+++ src/components/Team/Team.js
@@ -1,50 +1,96 @@
-import React from 'react'
-import teamMemberImage from "../../assets/team-member.jpg";
-import teamMember2Image from "../../assets/team-member2.jpg";
-import teamMember3Image from "../../assets/team-member3.jpg";
-import teamMember4Image from "../../assets/team-member4.jpg";
-import './team.css'
+import React from "react";
+import { Grid } from "@mui/material";
+import Andrei from "../../assets/team/andrei.jpg";
+import Raey from "../../assets/team/raey.jpg";
+import Khadija from "../../assets/team/khadija.jpg";
+import Sanidhya from "../../assets/team/sanidhya.jpg";
+import ruth from "../../assets/team/ruth.jpg";
+import Meri from "../../assets/team/meri.jpg";
+import Muslim from "../../assets/team/Muslim.jpg";
+import Elizaveta from '../../assets/team/elizaveta.jpg'
+import "./team.css";
 
-const
-    Team = () => {
+const TeamMember = ({ image, name, role }) => {
   return (
-      <section id="team-section" className="team-section">
-          <div className="text-container-team">
-            <h>OUR TEAM</h>
-          </div>
-          <div className="image-container-team">
-              <div className="team-member">
-                  <img src={teamMemberImage} alt="team-member 1"/>
-                  <div className="name-overlay">
-                      <h1>Dakota Johnson</h1>
-                      <p>actress</p>
-                  </div>
-              </div>
-              <div className="team-member">
-                  <img src={teamMember2Image} alt="team-member 2"/>
-                  <div className="name-overlay">
-                      <h1>Emma Watson</h1>
-                      <p>actress</p>
-                  </div>
-              </div>
-              <div className="team-member">
-                  <img src={teamMember3Image} alt="team-member 3"/>
-                  <div className="name-overlay">
-                      <h1>Some girl</h1>
-                      <p>random</p>
-                  </div>
-              </div>
-              <div className="team-member">
-                  <img src={teamMember4Image} alt="team-member 4"/>
-                  <div className="name-overlay">
-                      <h1>Daniel Radcliffe</h1>
-                      <p>actor</p>
-                  </div>
-              </div>
-              {/*Scale if you wish*/}
-          </div>
-      </section>
-  )
-}
+    <div className="team-member">
+      <img src={image} alt={`team-member ${name}`} />
+      <div className="name-overlay">
+        <h1>{name}</h1>
+        <p>{role}</p>
+      </div>
+    </div>
+  );
+};
 
-export default Team
+const Team = () => {
+  const teamMembers = [
+    {
+      image: Andrei,
+      name: "Andrei Bancila",
+      role: "Main Oraganisor",
+    },
+    {
+      image: "https://via.placeholder.com/300",
+      name: "Yasmine Ammouze",
+      role: "Main Oraganisor",
+    },
+    {
+      image: Raey,
+      name: "Raey Getahun",
+      role: "Head of IT",
+    },
+    {
+      image: ruth,
+      name: "Ruth Jebessa",
+      role: "Head of Logistics",
+    },
+    {
+      image: Khadija,
+      name: "Khadija Soua",
+      role: "Head of Design & Marketing ",
+    },
+    {
+      image: Sanidhya,
+      name: "Khadija Soua",
+      role: "Head of Design & Marketing ",
+    },
+    {
+      image: Elizaveta,
+      name: "Elizaveta Eremina",
+      role: "IT Team",
+    },
+    {
+      image: Meri,
+      name: "Meri Grigoryan",
+      role: "IT Team",
+    },
+    {
+      image: Muslim,
+      name: "Muslim Bayemirov",
+      role: "IT Team",
+    },
+  ];
+
+  return (
+    <section id="team-section" className="team-section">
+      <div className="text-container-team">
+        <h1>OUR TEAM</h1>
+      </div>
+      <div className="image-container-team">
+        <Grid container spacing={3}>
+          {teamMembers.map((member, index) => (
+            <Grid item xs={6} sm={3} md={4} key={index}>
+              <TeamMember
+                image={member.image}
+                name={member.name}
+                role={member.role}
+              />
+            </Grid>
+          ))}
+        </Grid>
+      </div>
+    </section>
+  );
+};
+
+export default Team;
