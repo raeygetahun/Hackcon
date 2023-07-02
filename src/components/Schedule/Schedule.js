ed712112c9d8d59d60f12b7b70d1d953ee751c22
raeygetahun <103925724+raeygetahun@users.noreply.github.com>
2023-07-02 14:21:30 +0200

    added Schedule

--- src/components/Schedule/Schedule.js
+++ src/components/Schedule/Schedule.js
@@ -1,7 +1,27 @@
-import React from "react";
+import React from 'react';
+import './schedule.css'; // Import the CSS file
 
 const Schedule = () => {
-  return <div></div>;
-};
+  const scheduleData = [
+    { time: '09:00 AM', event: 'Meeting with clients' },
+    { time: '11:00 AM', event: 'Team brainstorming session' },
+    { time: '01:00 PM', event: 'Lunch break' },
+    { time: '03:00 PM', event: 'Presentation preparation' },
+    { time: '05:00 PM', event: 'Project review meeting' },
+  ];
 
+  return (
+    <div className="schedule-container">
+      <h2 className="schedule-heading">Schedule</h2>
+      <ul className="schedule-list">
+        {scheduleData.map((item, index) => (
+          <li key={index} className="schedule-item">
+            <span className="time">{item.time}</span>
+            <span className="event">{item.event}</span>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 export default Schedule;
